859e67d413f04d8768eb1540ffb2f6ebd37e64d9
Ademaaa <florenlorence@gmail.com>
2019-04-10 22:40:56 -0300

    Teste faz tempo

--- facilitadorReceita/js/script.js
+++ facilitadorReceita/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
-	
+	$('#buscar').on('click', function(){
+		var baseURL = 'https://www.receitaws.com.br/v1/cnpj/';
+		var cnpj = $('#cnpj').val();
+		if(cnpj!='' && cnpj.length>=14){
+			
+			
+			$.getJSON( baseURL+cnpj, function(data) {
+				console.log( "success" );
+			}).done(function() {
+				console.log( "second success" );
+			})
+			.fail(function() {
+				console.log( "error" );
+			})
+			.always(function() {
+				console.log( "complete" );
+			});
+		}
+	});
 	
 });
